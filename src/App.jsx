50b9063ffe07592bca88b2dda8e398629845e391
Samit <samithossainmahadi@gmail.com>
2025-10-24 23:22:24 -0700

    setup and navbar is done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Sections/navbar'
+
+const App = () => {
+  return (
+    <div className='conatainer mx-auto max-w-7xl'>
+<Navbar />
+{/*hero*/}
+{/*about*/}
+{/*projects*/}
+{/*experience*/}
+{/*testimonial*/}
+{/*contact*/}
+{/*Footer*/}
+
+    </div>
+  )
+}
+
+export default App
