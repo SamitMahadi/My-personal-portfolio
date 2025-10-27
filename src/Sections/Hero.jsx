f5ff24a34ef330eef1ad8118ad0cee2ecf3a6610
Samit <samithossainmahadi@gmail.com>
2025-10-27 07:02:02 -0700

    3d model adjust done

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import HeroText from '../components/HeroText'
 import ParallexBg from '../components/ParallexBg'
+import { Canvas } from '@react-three/fiber'
+import { ThreeDModel } from '../components/threeDModel'
+import { OrbitControls } from '@react-three/drei'
+
+
 
 const Hero = () => {
   return (
-<section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
-<HeroText/>
-<ParallexBg />
+    <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
+      <HeroText />
+      <ParallexBg />
 
-<figure className='absloute inset-0' style={{width:'100vw',height:'100vh'}}>
+      <figure className='absloute inset-0 w-full h-full' style={{ width: '100vw', height: '100vh' }}>
+        <Canvas>
+          
+          <ThreeDModel />
+          <OrbitControls />
+        </Canvas>
+      </figure>
 
-</figure>
-
-</section>
+    </section>
   )
 }
 
