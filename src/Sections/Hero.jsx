885cb300ba587af3fb64356283aed97317c0fbf4
Samit <samithossainmahadi@gmail.com>
2025-10-27 11:25:32 -0700

    3d model done

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import HeroText from '../components/HeroText'
-import ParallexBg from '../components/ParallexBg'
-import { Canvas } from '@react-three/fiber'
-import { ThreeDModel } from '../components/threeDModel'
-import { OrbitControls } from '@react-three/drei'
-
-
+import { Canvas, useFrame } from "@react-three/fiber";
+import HeroText from "../components/HeroText";
+import ParallexBg from "../components/ParallexBg"
+import { ThreeDModel } from "../components/threeDModel";
+import { Float } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
+import { easing } from "maath";
+import { Suspense } from "react";
+// import Loader from "../components/Loader";
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
-    <section className='flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space'>
+    <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
       <HeroText />
       <ParallexBg />
-
-      <figure className='absloute inset-0 w-full h-full' style={{ width: '100vw', height: '100vh' }}>
-        <Canvas>
-          
-          <ThreeDModel />
-          <OrbitControls />
+      <figure
+        className="absolute inset-0"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas camera={{ position: [0, 1, 3] }}>
+          {/* <Suspense fallback={<Loader />}>
+            
+          </Suspense> */}
+          <Float>
+              <ThreeDModel
+                scale={isMobile && 0.23}
+                position={isMobile && [0, -1.5, 0]}
+              />
+            </Float>
+            <Rig />
         </Canvas>
       </figure>
-
     </section>
-  )
+  );
+};
+
+function Rig() {
+  return useFrame((state, delta) => {
+    easing.damp3(
+      state.camera.position,
+      [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+      0.5,
+      delta
+    );
+  });
 }
 
-export default Hero
+export default Hero;
